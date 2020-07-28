328fc25d4e71d2fea661fa1f3164fa1cf094dab7
Hizude <k.serkanertas@gmail.com>
2020-07-28 15:42:22 +0200

    added all functionality and styling. finished ver

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const keys = document.querySelectorAll('.flexbox-item');
+window.addEventListener('keydown', event => {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    console.log(event)
+    if (!audio) return;
+    audio.volume = 0.4;
+    audio.currentTime = 0;
+    audio.play();
 
+    const keyDisplay = document.querySelector('.keyDisplay');
+    keyDisplay.classList.remove('keyDisplay-before');
+    keyDisplay.classList.add('keyDisplay-after');
+    keyDisplay.innerText = event.key;
+})
